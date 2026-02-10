cb05357d3bbff09dc16ea2444e08216a317cd50a
Henry Kupty <hkupty@gmail.com>
2026-02-10 15:15:02 +0100

    refactor: Make `Step` the only entrypoint

--- pkg/core/engine.go
+++ pkg/core/engine.go
@@ -7,39 +7,21 @@ import (
 )
 
 func Step(state State, action any) (State, error) {
+	var nextState State
+	var err error
+
 	switch v := action.(type) {
 	case command.Walk:
-		return state.Move(v.Dir)
+		nextState, err = state.Move(v.Dir)
 	case command.Mark:
-		return state.ToggleMark(), nil
+		nextState = state.ToggleMark()
+	default:
+		return state, fmt.Errorf("unknown action type: %T", action)
 	}
 
-	return state, fmt.Errorf("unknown action type: %T", action)
-}
-
-func Process(state State, actions []any) (State, error) {
-
-	// NOTE: This is mostly for internal processing. While we don't want to re-implement this in TUI,
-	// we need to have a proper step-refresh screen synchrony, with a possible time delay (200~800ms)
-	// so the result is visible.
-
-	var stateCursor State
-	var err error
-	stateCursor = state
-	for _, action := range actions {
-		stateCursor, err = Step(stateCursor, action)
-		if err != nil {
-			return state, err
-		}
-		if stateCursor.IsAtFinish() {
-			return stateCursor, nil
-		}
+	if err != nil {
+		return state, err
 	}
 
-	if !stateCursor.IsAtFinish() {
-		return state, ErrIncompletePath
-	}
-
-	return stateCursor, nil
-
+	return nextState, nil
 }
